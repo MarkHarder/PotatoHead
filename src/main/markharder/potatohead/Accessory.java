20cad3c36ebd1bc7884b3dc4d0e509c8242bc74d
MarkHarder <mark.harder899@gmail.com>
2013-02-03 23:51:56 -0600

    Add accessories to inventory and image

---
+++ src/main/markharder/potatohead/Accessory.java
@@ -0,0 +1,53 @@
+package markharder.potatohead;
+
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
+/**
+ * Potato head parts
+ *
+ * @author Mark Harder
+ * @version 1.0
+ */
+public class Accessory extends Rectangle {
+    public static final int SIZE = 80;
+
+    private static BufferedImage tileset_features;
+    private int id;
+
+    static {
+        try {
+            tileset_features = ImageIO.read(new File("res/tileset_features.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create an accessory at (x, y) with dimensions (width, height) and an id
+     *
+     * @param x the x location on the screen
+     * @param y the y locatoin on the screen
+     * @param width the accessory width
+     * @param height the accessory height
+     * @param id which accessory it is
+     */
+    public Accessory(int x, int y, int width, int height, int id) {
+        setBounds(x, y, width, height);
+        this.id = id;
+    }
+
+    /**
+     * Paint the accessory
+     *
+     * @param g The graphics object drawn on
+     */
+    public void paint(Graphics g) {
+        g.drawImage(tileset_features, x, y, x + width, y + height, 0, id * SIZE, SIZE, SIZE + id * SIZE, null);
+    }
+}
+
