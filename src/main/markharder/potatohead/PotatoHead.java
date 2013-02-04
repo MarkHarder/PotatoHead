a2ca78c4c0250e5688c63714f4e4517367ecd9c6
MarkHarder <mark.harder899@gmail.com>
2013-02-03 23:11:38 -0600

    Set up a basic window

--- src/main/markharder/potatohead/PotatoHead.java
+++ src/main/markharder/potatohead/PotatoHead.java
@@ -2,6 +2,8 @@ package markharder.potatohead;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
 
 /**
  * A potato head accessorizor
@@ -15,15 +17,29 @@ public class PotatoHead extends JComponent {
     public PotatoHead() {
     }
 
+    public void paint(Graphics g) {
+        g.setColor(new Color(100, 10, 10));
+        g.fillRect(0, 0, 480, 600);
+    }
+
     /**
      * The entrypoint, create the frame, run the game
      *
      * @param args String arguments (unused)
      */
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
         spud = new PotatoHead();
 
-        System.out.println(spud);
+        // set up the window
+        frame.setUndecorated(true);
+        frame.add(spud);
+        frame.pack();
+        frame.setSize(480, 600);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
 
